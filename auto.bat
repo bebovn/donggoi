c88cbffa3156448d8bb210cb9ead0d9ceebe30ac
xdien <bdienw@gmail.com>
2018-01-26 02:00:31 +0700

    sua tu dong chay

---
+++ auto.bat
@@ -0,0 +1,4 @@
+#D:\Qt\QtIFW2.0.5\bin\repogen -e QtCoreLib,report --update -p packages dichvu
+#D:\Qt\QtIFW2.0.5\bin\repogen --update -e QtCoreLib,report -p packages kinhdoanh
+D:\Qt\QtIFW2.0.5\bin\repogen --update -e QtCoreLib,report -p packages nhansu
+#D:\Qt\QtIFW2.0.5\bin\repogen --update -p packages kinhdoanh
