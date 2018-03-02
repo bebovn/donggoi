bc677d01e44920907e21bd1ceeb4241fac373dcb
TonGiaBao <baoton488@gmail.com>
2018-03-02 15:48:23 +0700

    them file tu dong dong goi report and bin

---
+++ up_bin_report.bat
@@ -0,0 +1,3 @@
+xcopy /v/Y e:\nhansu\build\isuzu.exe e:\donggoi\packages\bin\data\
+xcopy "e:\nhansu\build\reports" "e:\donggoi\packages\report\data\reports" /s/h/e/k/f/c/Y
+e:\Qt\QtIFW2.0.5\bin\repogen --update -e QtCoreLib -p packages qtapp
