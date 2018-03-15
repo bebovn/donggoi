e4c27418ebe9d4ad67f423038085a871b28e853f
TonGiaBao <baoton488@gmail.com>
2018-03-15 07:56:38 +0700

    push dong goi

---
+++ up_bin - (bao).bat
@@ -0,0 +1,2 @@
+xcopy /v e:\nhansu\build\isuzu.exe e:\donggoi\packages\bin\data\
+e:\Qt\QtIFW2.0.5\bin\repogen --update -e QtCoreLib,report -p packages qtapp
--- up_bin_report.bat
+++ up_bin_report.bat
@@ -1,3 +1,3 @@
 xcopy /v/Y e:\nhansu\build\isuzu.exe e:\donggoi\packages\bin\data\
 xcopy "e:\nhansu\build\reports" "e:\donggoi\packages\report\data\reports" /s/h/e/k/f/c/Y
-e:\Qt\QtIFW2.0.5\bin\repogen --update -e QtCoreLib -p packages qtapp
+e:\Qt\QtIFW2.0.5\bin\repogen --update -p packages qtapp
